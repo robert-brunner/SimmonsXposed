4db8e44ef27ce76e71c7d321450c47318587c9fc
robert-brunner <brunner.robert.work@gmail.com>
2024-09-29 12:27:27 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import { Component } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/views/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
